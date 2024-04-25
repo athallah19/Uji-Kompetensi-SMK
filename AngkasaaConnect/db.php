e0f15a2ea176610cee7daa0a7e267e09155eb3e9
Athallah Abhi <127083737+athallah19@users.noreply.github.com>
2024-04-25 10:12:25 +0700

    Delete AngkasaaConnect directory

--- AngkasaaConnect/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'UKK_2024ATHALLAHABHIPRAMANA';
-
-$conn = mysqli_connect($hostname, $username, $password, $dbname) or die('gagal terhubung ke database');
-?>
